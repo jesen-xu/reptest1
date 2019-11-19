cf2de6bfb00be59092ab3e5b3c6931976d02767c
Jesen <466146530@qq.com>
2019-11-19 16:33:35 +0800

    了

--- manager/manager-web/src/main/java/com/mager/controller/PageController.java
+++ manager/manager-web/src/main/java/com/mager/controller/PageController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
     @RequestMapping("/{page}")
+
+
     public String showPage(@PathVariable String page) {
+        System.out.println("ss");
         return page;
     }
 }
